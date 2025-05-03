017f4783d36ca3f543c47536b68fde4e676ce787
KimBlazter <blasster35@gmail.com>
2025-05-03 14:53:24 +0200

    fix(Header): update header title from PokeIncremental to Click&Craft

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Link } from "@tanstack/react-router";
 export default function Header() {
     return (
         <div className="flex flex-row p-4 items-center bg-gray-900 fixed top-0 w-full">
-            <h1 className="font-semibold mr-auto">PokeIncremental</h1>
+            <h1 className="font-semibold mr-auto">Click&Craft</h1>
             <div className="flex flex-row items center space-x-4">
                 <Link to="/">Home</Link>
                 <Link to="/about">About</Link>
