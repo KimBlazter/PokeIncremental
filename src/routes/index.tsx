6b2db141b4d1008bc85f7d27c80936d45399994e
KimBlazter <blasster35@gmail.com>
2025-05-05 00:34:28 +0200

    refactor(Index): remove unused stores and simplify InventoryComponent usage

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,7 @@
 import { useGameLoop } from "@/hooks/useGameLoop";
-import { useResourceStore } from "@/stores/resources";
 import { createFileRoute } from "@tanstack/react-router";
-import Resources from "@/components/ResourcesComponent";
-import { useItemStore } from "@/stores/items";
 import InventoryComponent from "@/components/InventoryComponent";
+import MineWoodButton from "@/components/MineWoodButton";
 
 export const Route = createFileRoute("/")({
     component: Index,
@@ -11,31 +9,28 @@ export const Route = createFileRoute("/")({
 
 // IMPORTANT: the id "root" of the first div is mandatory.. else it will break the TanStack Router
 function Index() {
-    const itemStore = useItemStore();
-    const resourceStore = useResourceStore();
     const gameLoop = useGameLoop();
     return (
-        <div id="root" className="flex flex-row h-full w-full gap-4">
+        <div id="root" className="flex h-full w-full flex-row gap-4">
             {/* Left Panel */}
-            <div className="bg-green-400 h-full w-2/10 flex flex-col p-4">
+            <div className="flex h-full w-1/7 flex-col bg-green-400 p-4">
                 Left panel
             </div>
 
             {/* Central panel */}
-            <div className="bg-yellow-400 h-full flex flex-col flex-1">
+            <div className="flex h-full flex-1 flex-col bg-yellow-400">
                 {/* Central top */}
-                <div className="h-1/2 bg-purple-400"></div>
+                <div className="h-1/2 bg-purple-400">
+                    <MineWoodButton />
+                </div>
 
                 {/* Central bottom */}
                 <div className="h-1/2 bg-blue-400">
-                    <InventoryComponent
-                        items={itemStore.items}
-                        resources={resourceStore.resources}
-                    />
+                    <InventoryComponent />
                 </div>
             </div>
 
-            <div className="bg-red-400 h-full w-1/3 flex flex-col p-4">
+            <div className="flex h-full w-1/3 flex-col bg-red-400 p-4">
                 Left panel
             </div>
         </div>
