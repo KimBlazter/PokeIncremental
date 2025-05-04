ee7781bb14e73d28ef161c7d588804d63b6057d4
KimBlazter <blasster35@gmail.com>
2025-05-05 00:34:16 +0200

    feat(Route): add HeadContent component and update meta tags for improved SEO

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,15 +1,26 @@
-import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { createRootRoute, HeadContent, Outlet } from "@tanstack/react-router";
 
 import "@/index.css";
 import Header from "@/components/Header";
-
 export const Route = createRootRoute({
     component: () => (
         <>
+            <HeadContent />
             <Header />
-            <main className="h-screen pt-20 p-4">
+            <main className="h-screen pt-12">
                 <Outlet />
             </main>
         </>
     ),
+    head: () => ({
+        meta: [
+            {
+                name: "description",
+                content: "A minecraft inspired clicker game",
+            },
+            {
+                title: "Craft&Click",
+            },
+        ],
+    }),
 });
