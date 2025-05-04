2b254a20ed640c4899b1683aed3ea4c85ada13d6
KimBlazter <blasster35@gmail.com>
2025-05-05 00:34:36 +0200

    refactor(Header): simplify layout structure and improve readability

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,9 @@ import { Link } from "@tanstack/react-router";
 
 export default function Header() {
     return (
-        <div className="flex flex-row p-4 items-center bg-gray-900 fixed top-0 w-full">
-            <h1 className="font-semibold mr-auto">Click&Craft</h1>
-            <div className="flex flex-row items center space-x-4">
+        <div className="fixed top-0 flex h-12 w-full flex-row items-center bg-gray-900 p-4">
+            <h1 className="mr-auto font-semibold">Click&Craft</h1>
+            <div className="items center flex flex-row space-x-4">
                 <Link to="/">Home</Link>
                 <Link to="/about">About</Link>
             </div>
