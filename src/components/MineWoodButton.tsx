b75eca3cdc90b7976b11295743825042cc2678ce
KimBlazter <blasster35@gmail.com>
2025-05-08 12:21:09 +0200

    feat(MineWoodButton): use dynamic multiplier for wood resource addition

--- src/components/MineWoodButton.tsx
+++ src/components/MineWoodButton.tsx
@@ -2,5 +2,10 @@ import { useGameStore } from "@/stores/game";
 
 export default function MineWoodButton() {
     const addRessource = useGameStore((state) => state.addResource);
-    return <button onClick={() => addRessource("wood", 2)}>Mine Wood </button>;
+    const multiplier = useGameStore((state) => state.multiplier.wood);
+    return (
+        <button onClick={() => addRessource("wood", multiplier)}>
+            Mine Wood{" "}
+        </button>
+    );
 }
