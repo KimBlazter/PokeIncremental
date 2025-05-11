097403d656ac653198824b13a904a7486846a265
KimBlazter <blasster35@gmail.com>
2025-05-11 21:16:36 +0200

    fix(InventoryComponent): remove padding from item and resource containers

--- src/components/InventoryComponent.tsx
+++ src/components/InventoryComponent.tsx
@@ -3,14 +3,14 @@ import ItemsComponent from "@/components/ItemsComponent";
 
 export default function InventoryComponent() {
     return (
-        <div className="bg-mcInventoryBackground inventory-border text-mcInventoryText flex h-full flex-row gap-2">
+        <div className="inventory-border text-mcInventoryText flex h-full flex-row gap-2">
             {/* Resources */}
-            <div className="h-full w-3/7 p-2">
+            <div className="h-full w-3/7">
                 <ResourcesComponent />
             </div>
 
             {/* Items */}
-            <div className="h-full w-4/7 p-2">
+            <div className="h-full w-4/7">
                 <ItemsComponent />
             </div>
         </div>
