a87a2d25430757b52af04c4e668547547f612382
KimBlazter <blasster35@gmail.com>
2025-05-16 19:52:52 +0200

    feat(components): add texture model image and add tooltip support

--- src/components/ItemComponent.tsx
+++ src/components/ItemComponent.tsx
@@ -1,9 +1,19 @@
 import { Item } from "@/stores/items";
+import { getTextureFromIdentifier } from "@/utils/item-models";
+import clsx from "clsx";
 
 export default function ItemComponent({ item }: { item: Item }) {
     return (
-        <div className="item-slot flex items-end justify-end p-0.5 text-end text-xs">
-            {item.name}
+        <div className="item-slot relative flex items-center justify-center text-xs">
+            <div
+                aria-hidden
+                className={clsx(
+                    "icon-minecraft",
+                    getTextureFromIdentifier(
+                        item.textureIdentifier ?? "barrier"
+                    )
+                )}
+            />
         </div>
     );
 }
--- src/components/ResourceComponent.tsx
+++ src/components/ResourceComponent.tsx
@@ -1,15 +1,48 @@
-import { Resource } from "@/stores/resources";
+import { Resource, ResourceData } from "@/stores/resources";
+import { getTextureFromIdentifier } from "@/utils/item-models";
+import clsx from "clsx";
+import { formatNumber } from "@/utils/number-formatting-compact";
+import Decimal from "break_eternity.js";
+import { Tooltip } from "./Tooltip";
+import ResourceTooltipContent from "./Tooltips/ResourceTooltipContent";
 
 export default function ResourceComponent({
-    resource,
-    count,
+    resourceData,
+    resourceKey,
 }: {
-    resource: Resource;
-    count: number;
+    resourceData: ResourceData;
+    resourceKey: Resource;
 }) {
     return (
-        <div className="item-slot flex items-end justify-end p-0.5 text-end text-xs">
-            {resource} {count}
-        </div>
+        <Tooltip
+            content={
+                <ResourceTooltipContent
+                    resourceData={resourceData}
+                    resourceKey={resourceKey}
+                />
+            }
+        >
+            <div className="item-slot relative flex items-center justify-center text-xs">
+                <div
+                    aria-hidden
+                    className={clsx(
+                        "icon-minecraft",
+                        getTextureFromIdentifier(
+                            resourceData.texture_identifier
+                        )
+                    )}
+                />
+                <span
+                    className="text-mc mc-text-shadow absolute right-0 bottom-0 text-right text-sm"
+                    style={{
+                        lineHeight: "1",
+                        fontVariantLigatures: "none",
+                        fontKerning: "none",
+                    }}
+                >
+                    {formatNumber(Decimal.fromNumber(resourceData.amount))}
+                </span>
+            </div>
+        </Tooltip>
     );
 }
