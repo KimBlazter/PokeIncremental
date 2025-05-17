4b391a85da93a5d7243e6f42e100eeee58516df8
KimBlazter <blasster35@gmail.com>
2025-05-18 01:56:06 +0200

    feat(components): add effective tool icon in resource tooltip component

--- src/components/Tooltips/ResourceTooltipContent.tsx
+++ src/components/Tooltips/ResourceTooltipContent.tsx
@@ -1,7 +1,9 @@
 import { useGameStore } from "@/stores/game";
 import { Resource, ResourceData } from "@/stores/resources";
+import { getTextureFromIdentifier } from "@/utils/item-models";
 import { formatNumber } from "@/utils/number-formatting-compact";
 import Decimal from "break_eternity.js";
+import clsx from "clsx";
 
 export default function ResourceTooltipContent({
     resourceData,
@@ -24,7 +26,22 @@ export default function ResourceTooltipContent({
                     )}
                 </span>
             </span>
-            <span className="text-xs text-white/40">{perClick}/click</span>
+            <div className="flex flex-row items-center gap-1 text-sm text-white/30">
+                <span className="text-xs text-white/40">{perClick}/click</span>
+                (
+                <div
+                    aria-hidden
+                    className={clsx(
+                        "icon-minecraft-sm text-white opacity-30 brightness-0 invert-100",
+                        getTextureFromIdentifier(
+                            "iron_" + resourceData.effective_tool
+                        )
+                    )}
+                />
+                )
+            </div>
+
+            {}
         </div>
     );
 }
