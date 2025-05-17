2199ad8ac80c21eeb38b9435b0f1d05edf86541e
KimBlazter <blasster35@gmail.com>
2025-05-18 01:58:18 +0200

    feat(components): add MineResourceButton and remove MineWoodButton

---
+++ src/components/MineResourceButton.tsx
@@ -0,0 +1,14 @@
+import { useGameStore } from "@/stores/game";
+
+export default function MineResourceButton() {
+    const resource = useGameStore(
+        (state) => state.ages[state.currentAge].collected
+    );
+    const addRessource = useGameStore((state) => state.addResource);
+    const multiplier = useGameStore((state) => state.multiplier[resource]);
+    return (
+        <button onClick={() => addRessource(resource, multiplier)}>
+            Mine <span className="capitalize">{resource}</span>
+        </button>
+    );
+}
