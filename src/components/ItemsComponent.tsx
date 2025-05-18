d9c11a50dd7a1808e6fb759ca8b3585d3c434a4b
KimBlazter <blasster35@gmail.com>
2025-05-18 02:04:26 +0200

    fix(components): improve layout of ItemsComponent for better display

--- src/components/ItemsComponent.tsx
+++ src/components/ItemsComponent.tsx
@@ -4,9 +4,9 @@ import { useGameStore } from "@/stores/game";
 export default function ItemsComponent() {
     const items = useGameStore((state) => state.items);
     return (
-        <div className="flex flex-col">
+        <div className="flex h-full flex-col">
             <h2 className="text-mcInventoryText text-lg font-bold">Items</h2>
-            <div className="flex flex-row flex-wrap pt-1">
+            <div className="flex flex-row flex-wrap overflow-y-auto pt-1">
                 {Object.keys(items).map((id) => (
                     <ItemComponent key={id} item={items[id]} />
                 ))}
