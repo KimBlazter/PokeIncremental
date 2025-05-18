a2aa0b08af50ef9ed4c00f8f5a797d75813cf4e0
KimBlazter <blasster35@gmail.com>
2025-05-18 02:01:14 +0200

    feat(stores): enhance AgeSlice with collectible property and setCurrentAge method

--- src/stores/ages.ts
+++ src/stores/ages.ts
@@ -1,17 +1,21 @@
 import { StateCreator } from "zustand";
 import { produce } from "immer";
+import { Resource } from "./resources";
 
 export type AgeKey = "wood" | "stone" | "iron" | "gold";
 
 export type Age = {
     name: string;
     unlocked: boolean;
+    collectible: Resource;
 };
 
 export type Ages = Record<AgeKey, Age>;
 
 export interface AgeSlice {
     ages: Ages;
+    currentAge: AgeKey;
+    setCurrentAge: (age: AgeKey) => void;
     unlockAge: (age: AgeKey) => void;
 }
 
@@ -22,24 +26,35 @@ export const createAgeSlice: StateCreator<AgeSlice, [], [], AgeSlice> = (
         wood: {
             name: "Wood",
             unlocked: true,
+            collectible: "wood",
         },
         stone: {
             name: "Stone",
             unlocked: false,
+            collectible: "cobblestone",
         },
         iron: {
             name: "Iron",
             unlocked: false,
+            collectible: "iron",
         },
         gold: {
             name: "Gold",
             unlocked: false,
+            collectible: "gold",
         },
     },
+    currentAge: "wood",
     unlockAge: (age) =>
         set(
             produce((state: AgeSlice) => {
                 state.ages[age].unlocked = true;
+            })
+        ),
+    setCurrentAge: (age) =>
+        set(
+            produce((state: AgeSlice) => {
+                state.currentAge = age;
             })
         ),
 });
