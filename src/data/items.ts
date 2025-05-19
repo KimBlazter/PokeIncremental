bbe20eb91ae8195f41d0843b079d3756cd4e5df4
Petrequin Simon <simon.petrequin@etudiant.univ-rennes1.fr>
2025-05-19 03:13:35 +0200

    feat(items): add GAME_ITEMS definition for wooden and stone axes

---
+++ src/data/items.ts
@@ -0,0 +1,14 @@
+import { Item } from "@/stores/items";
+
+export const GAME_ITEMS: Record<string, Item> = {
+    wooden_axe: {
+        id: "wooden_axe",
+        name: "Wooden Axe",
+        textureIdentifier: "wooden_axe",
+    },
+    stone_axe: {
+        id: "stone_axe",
+        name: "Stone Axe",
+        textureIdentifier: "stone_axe",
+    },
+};
