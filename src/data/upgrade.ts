225e99ff78b32e25bef398dcce8cb7aa77648112
KimBlazter <blasster35@gmail.com>
2025-05-20 21:12:11 +0200

    refactor(upgrades): simplify upgrades structure and update effect functions to use gameStore

--- src/data/upgrade.ts
+++ src/data/upgrade.ts
@@ -1,36 +1,34 @@
-import { GameStore } from "@/stores/game";
 import { Upgrade } from "@/stores/upgrades";
 
-export const upgrades = (get: () => GameStore): Record<string, Upgrade> => {
-    return {
-        /*
+export const upgrades = {
+    /*
     WOOD AGE
     */
-        "upgrade.wood.0": {
-            name: "Big Biceps",
-            description: "wood +1",
-            cost: { resource: "wood", amount: 10 },
-            effect: () => get().updateMultiplier("wood", (prev) => prev + 1),
-            unlocked: false,
-        },
-        "unlock.stone": {
-            name: "Ready to get high",
-            description: "Unlock the Stone Age",
-            cost: { resource: "wood", amount: 10 },
-            effect: () => get().unlockAge("stone"),
-            unlocked: false,
-        },
+    "upgrade.wood.big_biceps": {
+        name: "Big Biceps",
+        description: "wood +1",
+        cost: { resource: "wood", amount: 10 },
+        effect: (gameStore) =>
+            gameStore.updateMultiplier("wood", (prev) => prev + 1),
+        unlocked: false,
+    },
+    "unlock.stone": {
+        name: "Ready to get high",
+        description: "Unlock the Stone Age",
+        cost: { resource: "wood", amount: 10 },
+        effect: (gameStore) => gameStore.unlockAge("stone"),
+        unlocked: false,
+    },
 
-        /*
+    /*
     STONE AGE
     */
-        "unlock.iron": {
-            name: "Harder than ever",
-            description: "Unlock the Iron Age",
-            cost: { resource: "cobblestone", amount: 20 },
-            effect: () => get().unlockAge("iron"),
-            unlocked: false,
-            ageRequirement: "stone",
-        },
-    };
-};
+    "unlock.iron": {
+        name: "Harder than ever",
+        description: "Unlock the Iron Age",
+        cost: { resource: "cobblestone", amount: 20 },
+        effect: (gameStore) => gameStore.unlockAge("iron"),
+        unlocked: false,
+        ageRequirement: "stone",
+    },
+} satisfies Record<string, Upgrade>;
