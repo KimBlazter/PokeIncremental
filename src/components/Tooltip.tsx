272e6e415f6ed967285a00c9d8cfdd232fc96cf2
KimBlazter <blasster35@gmail.com>
2025-05-22 21:34:44 +0200

    feat(components): improve tooltip positioning and alignment logic

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -6,17 +6,42 @@ export function Tooltip({
     content,
     className,
     position = "top",
+    align = "center",
 }: {
     children: ReactNode;
     content: ReactNode;
     className?: string;
     position?: "top" | "bottom" | "left" | "right";
+    align?: "start" | "center" | "end";
 }) {
-    const positionClasses = {
-        top: "bottom-full left-1/2 -translate-x-1/2 mb-2",
-        bottom: "top-full left-1/2 -translate-x-1/2 mt-2",
-        left: "right-full top-1/2 -translate-y-1/2 mr-2",
-        right: "left-full top-1/2 -translate-y-1/2 ml-2",
+    const basePosition = {
+        top: "bottom-full mb-2",
+        bottom: "top-full mt-2",
+        left: "right-full mr-2",
+        right: "left-full ml-2",
+    };
+
+    const alignment = {
+        top: {
+            start: "left-0",
+            center: "left-1/2 -translate-x-1/2",
+            end: "right-0",
+        },
+        bottom: {
+            start: "left-0",
+            center: "left-1/2 -translate-x-1/2",
+            end: "right-0",
+        },
+        left: {
+            start: "top-0",
+            center: "top-1/2 -translate-y-1/2",
+            end: "bottom-0",
+        },
+        right: {
+            start: "top-0",
+            center: "top-1/2 -translate-y-1/2",
+            end: "bottom-0",
+        },
     };
 
     return (
@@ -25,7 +50,8 @@ export function Tooltip({
             <div
                 className={clsx(
                     "tooltip-border absolute z-10 hidden px-2 py-1 text-xs whitespace-nowrap text-white opacity-0 shadow-lg transition-opacity group-hover:block group-hover:opacity-100",
-                    positionClasses[position]
+                    basePosition[position],
+                    alignment[position][align]
                 )}
             >
                 {content}
