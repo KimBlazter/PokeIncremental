e285ef7c8c1a6f4a7d8261d1579cb9e6a10d8298
KimBlazter <blasster35@gmail.com>
2025-05-22 21:38:03 +0200

    fix(data): add iconIndentifier field to data/ages

--- src/data/ages.ts
+++ src/data/ages.ts
@@ -5,20 +5,24 @@ export const ages = {
         name: "Wood",
         unlocked: true,
         collectible: "wood",
+        iconIdentifier: "oak_log",
     },
     stone: {
         name: "Stone",
         unlocked: false,
         collectible: "cobblestone",
+        iconIdentifier: "stone",
     },
     iron: {
         name: "Iron",
         unlocked: false,
         collectible: "iron",
+        iconIdentifier: "iron_block",
     },
     gold: {
         name: "Gold",
         unlocked: false,
         collectible: "gold",
+        iconIdentifier: "gold_block",
     },
 } satisfies Record<string, Age>;
