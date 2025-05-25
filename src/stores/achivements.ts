6ee0eca7423078c2a344514547747a661e3dde75
KimBlazter <blasster35@gmail.com>
2025-05-25 13:10:25 +0200

    feat(stores): :sparkles: implement achievement slice for managing game achievements

---
+++ src/stores/achivements.ts
@@ -0,0 +1,80 @@
+import { produce } from "immer";
+import { StateCreator } from "zustand";
+import { GameStore } from "./game";
+import { achievements } from "@/data/achievements";
+import { useToastStore } from "./toast";
+
+export type AchievementKey = keyof typeof achievements;
+
+export type Achievement = {
+    name: string;
+    parentId?: AchievementKey;
+    texture_identifier: string; // image identifier
+    condition: (gamestore: GameStore) => boolean;
+    unlocked: boolean;
+};
+
+export interface AchievementSlice {
+    achievements: Record<AchievementKey, Achievement>;
+    unlockAchievement: (id: AchievementKey) => void;
+    checkAchievements: () => void;
+    unlockables: AchievementKey[];
+    updateUnlockables: () => void;
+}
+
+export const createAchievementSlice: StateCreator<
+    GameStore,
+    [],
+    [],
+    AchievementSlice
+> = (set, get) => ({
+    achievements: achievements,
+    unlockAchievement: (achievementId) =>
+        set(
+            produce((state: AchievementSlice) => {
+                const ach = state.achievements[achievementId];
+
+                // check achievement isn't already unlocked
+                if (ach.unlocked) return;
+
+                // Do nothing if parent is not unlocked
+                if (ach.parentId && !state.achievements[ach.parentId].unlocked)
+                    return;
+
+                // Unlock achievement
+                useToastStore.getState().addToast({
+                    message: `achievement unlocked: ${ach.name}`,
+                    type: "success",
+                    duration: 5000,
+                });
+                state.achievements[achievementId].unlocked = true;
+            })
+        ),
+    checkAchievements: () => {
+        Object.keys(get().achievements).forEach((achievementKey) => {
+            const achievement =
+                get().achievements[achievementKey as AchievementKey];
+            if (achievement.condition(get()))
+                get().unlockAchievement(achievementKey as AchievementKey);
+        });
+    },
+    unlockables: [],
+    updateUnlockables: () =>
+        set(
+            produce((state: AchievementSlice) => {
+                state.unlockables = Object.keys(state.achievements).filter(
+                    (key) => {
+                        const ach = state.achievements[key as AchievementKey];
+                        // Only consider achievements that aren't already unlocked
+                        if (ach.unlocked) return false;
+
+                        // If it has a parent, the parent must be unlocked
+                        return (
+                            !ach.parentId ||
+                            state.achievements[ach.parentId].unlocked
+                        );
+                    }
+                ) as AchievementKey[];
+            })
+        ),
+});
