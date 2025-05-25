cbb94d0cc20e70392d6bf48e0a48750bfc855de4
KimBlazter <blasster35@gmail.com>
2025-05-25 13:10:42 +0200

    feat(achievements): :sparkles: add achievements data

---
+++ src/data/achievements.ts
@@ -0,0 +1,16 @@
+import { Achievement } from "@/stores/achivements";
+
+export const achievements = {
+    "stone.stoner": {
+        name: "Stoner",
+        texture_identifier: "stone_block",
+        condition: (gamestore) => gamestore.resources.cobblestone.amount >= 42,
+        unlocked: false,
+    },
+    "iron.man_of_steel": {
+        name: "Man of Steel",
+        texture_identifier: "iron_nugget",
+        condition: (gamestore) => gamestore.resources.iron.amount >= 100,
+        unlocked: false,
+    },
+} satisfies Record<string, Achievement>;
