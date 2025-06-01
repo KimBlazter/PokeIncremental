7315ac39e4de459bbfd85225901379c69bf8f6cf
KimBlazter <blasster35@gmail.com>
2025-06-02 00:51:40 +0200

    chore(components): :truck: move settings button to the settings folder

--- src/components/settings/SettingsButton.tsx
+++ src/components/settings/SettingsButton.tsx
@@ -1,19 +1,22 @@
 import { useState } from "react";
 import Modal from "@/components/ui/Modal";
 import SaveManager from "../save/SaveManager";
+import HotkeySettings from "./HotkeySettings";
 
 export default function SettingsButton() {
     const [open, setOpen] = useState(false);
     return (
         <div className="w-full">
             <button
-                onClick={() => setOpen(true)}
+                onClick={() => setOpen(!open)}
                 className="w-full !text-white"
+                id="open-settings-button"
             >
                 Settings
             </button>
             <Modal isOpen={open} onClose={() => setOpen(false)}>
                 <h2 className="mb-12 text-xl font-bold">Settings</h2>
+                <HotkeySettings />
                 <SaveManager />
             </Modal>
         </div>
