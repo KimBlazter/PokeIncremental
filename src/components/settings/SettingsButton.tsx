456e4ad29ea35b7ed8feb087298f0f9707dd640e
KimBlazter <blasster35@gmail.com>
2025-06-02 20:01:53 +0200

    refactor(settings): replace local state management with useModalStore for settings modal

--- src/components/settings/SettingsButton.tsx
+++ src/components/settings/SettingsButton.tsx
@@ -1,23 +1,27 @@
-import { useState } from "react";
 import Modal from "@/components/ui/Modal";
 import SaveManager from "../save/SaveManager";
 import HotkeySettings from "./HotkeySettings";
+import ResetSave from "./ResetSave";
+import { useModalStore } from "@/stores/modals";
 
 export default function SettingsButton() {
-    const [open, setOpen] = useState(false);
+    const toggle = useModalStore((s) => s.toggle);
     return (
         <div className="w-full">
             <button
-                onClick={() => setOpen(!open)}
+                onClick={() => toggle("settings-modal")}
                 className="w-full !text-white"
                 id="open-settings-button"
             >
                 Settings
             </button>
-            <Modal isOpen={open} onClose={() => setOpen(false)}>
+            <Modal modalId="settings-modal">
                 <h2 className="mb-12 text-xl font-bold">Settings</h2>
                 <HotkeySettings />
                 <SaveManager />
+                <div className="mt-10 flex w-full justify-end">
+                    <ResetSave />
+                </div>
             </Modal>
         </div>
     );
