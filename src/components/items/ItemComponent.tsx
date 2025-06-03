6ee6f98483fb8a4c82fbae1cd095728ff7bbfdfa
Petrequin Simon <simon.petrequin@etudiant.univ-rennes1.fr>
2025-06-04 01:24:34 +0200

    feat:   left click on item to equip it

--- src/components/items/ItemComponent.tsx
+++ src/components/items/ItemComponent.tsx
@@ -3,15 +3,20 @@ import { getTextureFromIdentifier } from "@/utils/item-models";
 import clsx from "clsx";
 import { Tooltip } from "@/components/ui/Tooltip";
 import ItemTooltipContent from "@/components/Tooltips/ItemTooltipContent";
+import { useGameStore } from "@/stores/game";
 
 export default function ItemComponent({ item }: { item: Item }) {
+    const equipItem = useGameStore((state) => state.equipItem);
     return (
         <Tooltip
             content={<ItemTooltipContent item={item} />}
             position="top"
             align="start"
         >
-            <div className="item-slot relative flex items-center justify-center text-xs">
+            <div
+                className="item-slot relative flex items-center justify-center text-xs"
+                onClick={() => equipItem(item)}
+            >
                 <div
                     aria-hidden
                     className={clsx(
