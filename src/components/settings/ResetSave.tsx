9870a271d631426fe50565578579f186a6cccae8
KimBlazter <blasster35@gmail.com>
2025-06-04 00:00:04 +0200

    fix(ResetSave): add confirmation dialog before resetting save data

--- src/components/settings/ResetSave.tsx
+++ src/components/settings/ResetSave.tsx
@@ -2,6 +2,12 @@ import clsx from "clsx";
 
 export default function ResetSave({ className }: { className?: string }) {
     const handleResetSave = () => {
+        if (
+            !window.confirm(
+                "WARNING: All data will be permanently wiped. Are you sure you want to reset your save?"
+            )
+        )
+            return;
         // Wiping save data from localstorage
         localStorage.clear();
         console.log("Save data has been reset successfully");
