4bca6941964715be87b18b5d91dc22bf53428437
KimBlazter <blasster35@gmail.com>
2025-06-04 23:44:23 +0200

    feat(components): add ArmorsHotbar component for managing armor equipment

---
+++ src/components/equipments/ArmorsHotbar.tsx
@@ -0,0 +1,33 @@
+import { SlotType } from "@/stores/equipments";
+import { useGameStore } from "@/stores/game";
+import EquipmentSlot from "./EquipmentSlot";
+import clsx from "clsx";
+
+export const toolsSlots: SlotType[] = [
+    "helmet",
+    "chestplate",
+    "leggings",
+    "boots",
+    "shield",
+];
+
+export default function ArmorsHotbar({ className }: { className?: string }) {
+    const equipmentSlots = useGameStore((state) => state.equipments);
+    return (
+        <div
+            className={clsx(
+                "inventory-border flex flex-col border-l-0 pl-2",
+                className
+            )}
+        >
+            {toolsSlots.map((slotType) => (
+                <EquipmentSlot
+                    key={slotType}
+                    slotType={slotType}
+                    item={equipmentSlots[slotType]}
+                    tooltipPosition="right"
+                />
+            ))}
+        </div>
+    );
+}
