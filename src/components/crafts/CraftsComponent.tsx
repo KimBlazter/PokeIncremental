2ea1223791b6a27e2ccfe1fb426b27412e7fbfc4
Petrequin Simon <simon.petrequin@etudiant.univ-rennes1.fr>
2025-06-04 16:22:35 +0200

    refactor: fix weird crafts dispositoin

--- src/components/crafts/CraftsComponent.tsx
+++ src/components/crafts/CraftsComponent.tsx
@@ -5,10 +5,12 @@ import { CraftKey } from "@/stores/crafts";
 export default function CraftsComponent() {
     const crafts = useGameStore((state) => state.crafts);
     return (
-        <div className="flex h-full w-full flex-row flex-wrap">
-            {Object.keys(crafts).map((craft) => (
-                <CraftComponent key={craft} craftId={craft as CraftKey} />
-            ))}
+        <div className="flex h-full w-full flex-col">
+            <div className="flex flex-row flex-wrap">
+                {Object.keys(crafts).map((craft) => (
+                    <CraftComponent key={craft} craftId={craft as CraftKey} />
+                ))}
+            </div>
         </div>
     );
 }
