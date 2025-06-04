104ca2339298cc1cc65a608d6069995f5e303a1b
KimBlazter <blasster35@gmail.com>
2025-06-04 23:31:40 +0200

    feat(components): add ToolsHotbar component for equipment management

---
+++ src/components/equipments/ToolsHotbar.tsx
@@ -0,0 +1,32 @@
+import { SlotType } from "@/stores/equipments";
+import { useGameStore } from "@/stores/game";
+import EquipmentSlot from "./EquipmentSlot";
+import clsx from "clsx";
+
+export const toolsSlots: SlotType[] = [
+    "sword",
+    "axe",
+    "pickaxe",
+    "shovel",
+    "hoe",
+];
+
+export default function ToolsHotbar({ className }: { className?: string }) {
+    const equipmentSlots = useGameStore((state) => state.equipments);
+    return (
+        <div
+            className={clsx(
+                "inventory-border flex flex-col border-r-0 pr-2",
+                className
+            )}
+        >
+            {toolsSlots.map((slotType) => (
+                <EquipmentSlot
+                    key={slotType}
+                    slotType={slotType}
+                    item={equipmentSlots[slotType]}
+                />
+            ))}
+        </div>
+    );
+}
