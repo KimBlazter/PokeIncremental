e517ec8e66b7857b88afc9e772ccf4aed122029f
Petrequin Simon <simon.petrequin@etudiant.univ-rennes1.fr>
2025-06-05 03:22:40 +0200

    feat(achievements): updated existing achivements for hint and desc + added 1 ach

--- src/data/achievements.ts
+++ src/data/achievements.ts
@@ -2,15 +2,29 @@ import { Achievement } from "@/stores/achivements";
 import { AgeKey } from "@/stores/ages";
 
 export const achievements = {
+    "wood.how_many_more": {
+        name: "How many more ???",
+        texture_identifier: "wooden_axe",
+        hint: "Craft way to much items",
+        description: "Have 4 Wooden Axe in your inventory",
+        condition: (gamestore) =>
+            gamestore.items.filter((item) => item.id === "wooden_axe").length >=
+            4,
+        unlocked: false,
+    },
     "stone.stoner": {
         name: "Stoner",
         texture_identifier: "stone",
+        hint: "Keep mining...",
+        description: "Have 42 cobblestone or more",
         condition: (gamestore) => gamestore.resources.cobblestone.amount >= 42,
         unlocked: false,
     },
     "iron.man_of_steel": {
         name: "Man of Steel",
         parentId: "stone.stoner",
+        hint: "We need iron for the golem",
+        description: "Have 100 iron or more",
         texture_identifier: "iron_nugget",
         condition: (gamestore) => gamestore.resources.iron.amount >= 100,
         unlocked: false,
