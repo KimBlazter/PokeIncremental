c80cd5efb0503bc9a0be5bae90db81f5271bb9c5
KimBlazter <blasster35@gmail.com>
2025-06-07 22:16:37 +0200

    refactor(components): replace texture rendering with ItemIcon component

--- src/components/items/ItemComponent.tsx
+++ src/components/items/ItemComponent.tsx
@@ -1,12 +1,12 @@
 import { Item } from "@/stores/items";
-import { getTextureFromIdentifier } from "@/utils/item-models";
-import clsx from "clsx";
 import { Tooltip } from "@/components/ui/Tooltip";
 import ItemTooltipContent from "@/components/Tooltips/ItemTooltipContent";
 import { useGameStore } from "@/stores/game";
+import ItemIcon from "../ItemIcon";
 
 export default function ItemComponent({ item }: { item: Item }) {
     const equipItem = useGameStore((state) => state.equipItem);
+
     return (
         <Tooltip
             content={<ItemTooltipContent item={item} />}
@@ -17,15 +17,8 @@ export default function ItemComponent({ item }: { item: Item }) {
                 className="item-slot relative flex items-center justify-center text-xs"
                 onClick={() => equipItem(item)}
             >
-                <div
-                    aria-hidden
-                    className={clsx(
-                        "icon-minecraft",
-                        getTextureFromIdentifier(
-                            item.textureIdentifier ?? "barrier"
-                        )
-                    )}
-                />
+                <ItemIcon textureIdentifier={item.textureIdentifier} />
+                {false && <div className="enchanted" />}
             </div>
         </Tooltip>
     );
