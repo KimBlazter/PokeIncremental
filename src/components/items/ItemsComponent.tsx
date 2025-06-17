d3fa1350618340935857f0882a654910be6cc862
KimBlazter <blasster35@gmail.com>
2025-06-17 19:45:26 +0200

    refactor(components): enhance ItemsComponent to calculate and display empty slots

--- src/components/items/ItemsComponent.tsx
+++ src/components/items/ItemsComponent.tsx
@@ -2,14 +2,23 @@ import ItemComponent from "./ItemComponent";
 import { useGameStore } from "@/stores/game";
 import { v4 as uuidv4 } from "uuid";
 
+const MIN_VISIBLE_SLOTS = 16;
+
 export default function ItemsComponent() {
     const items = useGameStore((state) => state.items);
+
+    const emptySlotsCount = Math.max(0, MIN_VISIBLE_SLOTS - items.length);
+
     return (
         <div className="flex h-full flex-col">
             <h2 className="text-mcInventoryText text-lg font-bold">Items</h2>
             <div className="flex flex-row flex-wrap overflow-y-auto pt-1">
                 {items.map((item) => (
                     <ItemComponent key={uuidv4()} item={item} />
+                ))}
+                {/* Empty slots */}
+                {Array.from({ length: emptySlotsCount }).map((_, idx) => (
+                    <div key={idx} className="item-slot" />
                 ))}
             </div>
         </div>
