fbebd468b251886ec001edeacd4b6ceae1a84653
KimBlazter <blasster35@gmail.com>
2025-06-17 19:38:38 +0200

    chore(stores): :truck: rename multiplier store to bonuses store

---
+++ src/stores/bonuses.ts
@@ -0,0 +1,119 @@
+import { StateCreator } from "zustand";
+import { Resource } from "./resources";
+import { produce } from "immer";
+import { GameStore } from "./game";
+
+interface Bonus {
+    baseGain: number;
+    multiplier: number;
+}
+type ResourceBonus = Record<Resource, Bonus>;
+
+interface TimedBonus {
+    multiplier?: number;
+    baseGain?: number;
+    expiresAt: string; // timestamp ISO string /!\ important /!\
+    source?: string; // e.g., "powerup", "skill", "item"
+}
+type ResourceTimedBonus = Record<Resource, TimedBonus[]>;
+
+export interface BonusSlice {
+    permanentBonuses: ResourceBonus;
+    timedBonuses: ResourceTimedBonus;
+    updateMultiplier: (
+        resource: Resource,
+        value: number | ((current: number) => number)
+    ) => void;
+    updateBaseGain: (
+        resource: Resource,
+        value: number | ((current: number) => number)
+    ) => void;
+    addTimedBonus: (resource: Resource, bonus: TimedBonus) => void;
+    cleanupExpiredBonuses: () => void; // This function should handle the removal of expired modifiers
+    computeResourcesYield: (resource: Resource) => number;
+}
+
+export const createBonusSlice: StateCreator<GameStore, [], [], BonusSlice> = (
+    set,
+    get
+) => ({
+    permanentBonuses: {
+        wood: { baseGain: 1, multiplier: 1 },
+        cobblestone: { baseGain: 1, multiplier: 1 },
+        iron: { baseGain: 1, multiplier: 1 },
+        gold: { baseGain: 1, multiplier: 1 },
+    },
+    timedBonuses: {
+        wood: [],
+        cobblestone: [],
+        iron: [],
+        gold: [],
+    },
+    updateBaseGain: (resource, value) =>
+        set(
+            produce((state: BonusSlice) => {
+                state.permanentBonuses[resource].baseGain =
+                    typeof value == "number" ? value : (
+                        value(state.permanentBonuses[resource].baseGain)
+                    );
+            })
+        ),
+    updateMultiplier: (resource, value) =>
+        set(
+            produce((state: BonusSlice) => {
+                state.permanentBonuses[resource].multiplier =
+                    typeof value == "number" ? value : (
+                        value(state.permanentBonuses[resource].multiplier)
+                    );
+            })
+        ),
+    addTimedBonus: (resource, bonus) =>
+        set(
+            produce((state: BonusSlice) => {
+                state.timedBonuses[resource].push(bonus);
+            })
+        ),
+    cleanupExpiredBonuses: () =>
+        set(
+            produce((state: BonusSlice) => {
+                Object.entries(state.timedBonuses).forEach(
+                    ([resource, bonuses]) => {
+                        bonuses.forEach((bonus) => {
+                            if (new Date(bonus.expiresAt) < new Date()) {
+                                // Remove expired bonus
+                                state.timedBonuses[resource as Resource] =
+                                    state.timedBonuses[
+                                        resource as Resource
+                                    ].filter((b) => b !== bonus);
+                            }
+                        });
+                    }
+                );
+            })
+        ),
+    computeResourcesYield: (resource) => {
+        const state = get();
+        // Ensure all the bonuses are cleaned up before computing the yield
+        state.cleanupExpiredBonuses();
+
+        const activeTimedBonuses = state.timedBonuses[resource];
+
+        // Compute flat bonus (base + temporaries)
+        const totalFlatBonus =
+            state.permanentBonuses[resource].baseGain +
+            activeTimedBonuses.reduce(
+                (acc, timedBonus) => acc + (timedBonus.baseGain ?? 0),
+                0
+            );
+
+        // Compute multiplier (base * temporaries)
+        const totalMultiplier =
+            state.permanentBonuses[resource].multiplier *
+            activeTimedBonuses.reduce(
+                (acc, timedBonus) => acc * (timedBonus.multiplier ?? 1),
+                1
+            );
+
+        return totalFlatBonus * totalMultiplier;
+    },
+});
