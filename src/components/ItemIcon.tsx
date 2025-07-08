0be687ddc012767960b382c89915abf7175803ab
KimBlazter <blasster35@gmail.com>
2025-07-08 19:21:35 +0200

    refactor(components): update ItemIcon to use texture prop and load texture dynamically

--- src/components/ItemIcon.tsx
+++ src/components/ItemIcon.tsx
@@ -1,21 +1,49 @@
+import { useState, useEffect } from "react";
 import { getTextureFromIdentifier } from "@/utils/item-models";
+import getTexture, { Texture } from "@/utils/spriteLoader";
 import clsx from "clsx";
 
 export default function ItemIcon({
-    textureIdentifier,
+    texture,
     className,
     style,
 }: {
-    textureIdentifier?: string;
+    texture?: Texture;
     className?: string;
     style?: React.CSSProperties;
 }) {
+    const [textureDataURL, setTextureDataURL] = useState<string | null>(null);
+
+    useEffect(() => {
+        if (texture) {
+            getTexture(texture)
+                .then(setTextureDataURL)
+                .catch((error) => {
+                    console.error(
+                        `Failed to load texture for ${texture}:`,
+                        error
+                    );
+                });
+        }
+    }, [texture]);
+
+    if (textureDataURL) {
+        return (
+            <img
+                src={textureDataURL}
+                alt=""
+                className={clsx("aspect-square p-1", className)}
+                style={style}
+            />
+        );
+    }
+
     return (
         <div
             aria-hidden
             className={clsx(
                 "icon-minecraft aspect-square",
-                getTextureFromIdentifier(textureIdentifier ?? "barrier"),
+                getTextureFromIdentifier("barrier"),
                 className
             )}
             style={style}
