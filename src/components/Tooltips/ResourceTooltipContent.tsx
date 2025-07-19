642ed3bdd910703ee37a52c034f2b9187ffb48cf
KimBlazter <blasster35@gmail.com>
2025-07-19 12:38:30 +0200

    refactor(tooltip): :recycle: simplify amount formatting and update ItemIcon class

--- src/components/Tooltips/ResourceTooltipContent.tsx
+++ src/components/Tooltips/ResourceTooltipContent.tsx
@@ -1,7 +1,6 @@
 import { useGameStore } from "@/stores/game";
 import { Resource, ResourceData } from "@/stores/resources";
 import { formatNumber } from "@/utils/number-formatting-compact";
-import Decimal from "break_eternity.js";
 import ItemIcon from "../ItemIcon";
 import { TextureId } from "@/utils/spriteLoader";
 
@@ -23,18 +22,13 @@ export default function ResourceTooltipContent({
             <span>
                 {resourceData.name}{" "}
                 <span className="text-sm opacity-60">
-                    x
-                    {formatNumber(
-                        Decimal.fromNumber(resourceData.amount),
-                        3,
-                        6
-                    )}
+                    x{formatNumber(resourceData.amount, 3, 6)}
                 </span>
             </span>
             <div className="flex flex-row items-center gap-1 text-sm text-white/30">
                 <span className="text-xs text-white/40">{perClick}/click</span>(
                 <ItemIcon
-                    className="icon-minecraft-sm text-white opacity-30 brightness-0 invert-100"
+                    className="-m-2 size-6 text-white opacity-30 brightness-0 invert-100"
                     texture={
                         ("item:iron_" +
                             resourceData.effective_tool) as TextureId
