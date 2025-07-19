168a8c4bbe775da49fc4f52b4ac8c4438b1f0eb3
KimBlazter <blasster35@gmail.com>
2025-07-19 12:39:21 +0200

    feat(resources): :sparkles: enhance resource definitions with obtainedFrom and hp properties

--- src/data/resources.ts
+++ src/data/resources.ts
@@ -1,28 +1,41 @@
 import { ResourceData } from "@/stores/resources";
+import { GAME_ITEMS } from "./items";
 
 export const resources = {
     wood: {
         name: "Wood",
         amount: 0,
         texture: "block:oak_log",
+        obtainedFrom: { ...GAME_ITEMS.oak_log },
+        hp: 100,
         effective_tool: "axe",
     },
     cobblestone: {
         name: "Cobblestone",
         amount: 0,
         texture: "block:cobblestone",
+        obtainedFrom: { ...GAME_ITEMS.stone },
+        hp: 250,
         effective_tool: "pickaxe",
     },
     iron: {
         name: "Iron",
         amount: 0,
         texture: "item:raw_iron",
+        obtainedFrom: {
+            ...GAME_ITEMS.iron_ore,
+        },
+        hp: 600,
         effective_tool: "pickaxe",
     },
     gold: {
         name: "Gold",
         amount: 0,
         texture: "item:raw_gold",
+        obtainedFrom: {
+            ...GAME_ITEMS.gold_ore,
+        },
+        hp: 800,
         effective_tool: "pickaxe",
     },
 } satisfies Record<string, ResourceData>;
